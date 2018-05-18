ef59960c1021b0e6f78b6cc91dd1de7c8703944f
Rob Rix <rob.rix@me.com>
2018-05-17 21:01:05 -0400

    Move zero down.

--- src/Data/Semiring/Class.hs
+++ src/Data/Semiring/Class.hs
@@ -1,12 +1,6 @@
 -- | 'Semiring's, with and without multiplicative identities.
 module Data.Semiring.Class where
 
--- | The identity element of some 'Monoid' @m@.
---
---   If @m@ is additionally a 'Semiring', this is its additive identity.
-zero :: Monoid m => m
-zero = mempty
-
 -- | A 'Semiring' @r@ is an abstract algebraic structure consisting of a commutative 'Semigroup' and an associative operator '><', with the additional constraints that '><' distributes over '<>'. Additionally, if @s@ is a 'Monoid', 'zero' is the absorping element of '><'.
 --
 --   Laws:
@@ -52,6 +46,13 @@ zero = mempty
 class Semigroup r => Semiring r where
   infixr 7 ><
   (><) :: r -> r -> r
+
+
+-- | The identity element of some 'Monoid' @m@.
+--
+--   If @m@ is additionally a 'Semiring', this is its additive identity.
+zero :: Monoid m => m
+zero = mempty
 
 
 -- | 'Unital' 'Semiring's additionally have a multiplicative identity, 'one'.
