f14784061ce8e2bcf6e9d96cbacb8a59f99781e4
Rob Rix <rob.rix@me.com>
2018-05-18 09:21:47 -0400

    Define a datatype for the tropical semiring.

--- src/Data/Semiring/Tropical.hs
+++ src/Data/Semiring/Tropical.hs
@@ -1,1 +1,8 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.Tropical where
+
+import Data.Data (Data(..))
+import GHC.Generics (Generic, Generic1)
+
+data Tropical n = Finite n | Infinity
+  deriving (Data, Eq, Generic, Generic1, Ord, Read, Show)
