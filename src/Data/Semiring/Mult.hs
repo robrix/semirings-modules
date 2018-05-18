e9da4a10c0cdb1f7778542413f7a34e04c1fab9a
Rob Rix <rob.rix@me.com>
2018-05-17 21:24:09 -0400

    Define a MonadFix instance for Mult.

--- src/Data/Semiring/Mult.hs
+++ src/Data/Semiring/Mult.hs
@@ -2,7 +2,9 @@
 module Data.Semiring.Mult where
 
 import Control.Applicative (Applicative(..))
+import Control.Monad.Fix (MonadFix(..))
 import Data.Coerce (coerce)
+import Data.Function (fix)
 
 newtype Mult r = Mult { getMult :: r }
   deriving (Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
@@ -17,3 +19,6 @@ instance Applicative Mult where
 instance Monad Mult where
   (>>) = (*>)
   Mult a >>= f = f a
+
+instance MonadFix Mult where
+  mfix f = fix (f . getMult)
