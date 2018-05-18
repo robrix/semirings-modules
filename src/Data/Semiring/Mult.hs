f106a650667c7bc9080c786e5b252dec3a528c90
Rob Rix <rob.rix@me.com>
2018-05-17 21:18:48 -0400

    Define an Applicative instance for Mult.

--- src/Data/Semiring/Mult.hs
+++ src/Data/Semiring/Mult.hs
@@ -3,3 +3,7 @@ module Data.Semiring.Mult where
 
 newtype Mult r = Mult { getMult :: r }
   deriving (Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
+
+instance Applicative Mult where
+  pure = Mult
+  Mult f <*> Mult a = Mult (f a)
