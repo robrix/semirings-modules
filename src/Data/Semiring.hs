503d6159c7bace4b37050b2cad6153fe05edc85b
Rob Rix <rob.rix@me.com>
2018-05-18 09:19:53 -0400

    Stub in a module for the tropical numbers.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -3,8 +3,10 @@ module Data.Semiring
 ( module Data.Semiring.Arith
 , module Data.Semiring.Class
 , module Data.Semiring.Mult
+, module Data.Semiring.Tropical
 ) where
 
 import Data.Semiring.Arith
 import Data.Semiring.Class
 import Data.Semiring.Mult
+import Data.Semiring.Tropical
