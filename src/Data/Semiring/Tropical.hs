28796b64a7d91a9bcdb78144e58a20e32073c620
Rob Rix <rob.rix@me.com>
2018-05-18 09:31:50 -0400

    Define a Num instance for Tropical.

--- src/Data/Semiring/Tropical.hs
+++ src/Data/Semiring/Tropical.hs
@@ -6,3 +6,18 @@ import GHC.Generics (Generic, Generic1)
 
 data Tropical n = Finite n | Infinity
   deriving (Data, Eq, Generic, Generic1, Ord, Read, Show)
+
+instance Real r => Num (Tropical r) where
+  Finite a + Finite b = Finite (a `min` b)
+  a        + Infinity = a
+  Infinity + b        = b
+  Finite a * Finite b = Finite (a + b)
+  Infinity * _        = Infinity
+  _        * Infinity = Infinity
+  negate (Finite a) = Finite (negate a)
+  negate Infinity   = Infinity
+  abs    (Finite a) = Finite (abs    a)
+  abs    Infinity   = Infinity
+  signum (Finite a) = Finite (signum a)
+  signum Infinity   = 1
+  fromInteger       = Finite . fromInteger
