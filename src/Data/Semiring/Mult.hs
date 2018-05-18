f52937ed3812cbbdff4dcf74725efde7c37673a2
Rob Rix <rob.rix@me.com>
2018-05-17 21:18:18 -0400

    Derive a bunch of instances.

--- src/Data/Semiring/Mult.hs
+++ src/Data/Semiring/Mult.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveFoldable, DeriveFunctor, DeriveTraversable, GeneralizedNewtypeDeriving #-}
 module Data.Semiring.Mult where
 
 newtype Mult r = Mult { getMult :: r }
+  deriving (Enum, Eq, Foldable, Functor, Num, Ord, Read, Show, Traversable)
