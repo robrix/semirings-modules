1c1e1f6507751f7f74895f4c5a7a27a79e8235c2
Rob Rix <rob.rix@me.com>
2018-05-17 20:36:12 -0400

    Reformat the docs.

--- src/Data/Semiring/Class.hs
+++ src/Data/Semiring/Class.hs
@@ -12,30 +12,42 @@ zero = mempty
 --
 --   Associativity of '<>' (the 'Semigroup' law):
 --
---   > a <> (b <> c) = (a <> b) <> c
+-- @
+--   a '<>' (b '<>' c) = (a '<>' b) '<>' c
+-- @
 --
 --   Identity of '<>' (the 'Monoid' law, if @s@ is a 'Monoid'):
 --
---   > zero <> a = a
---   > a <> zero = a
+-- @
+--   'zero' '<>' a = a
+--   a '<>' 'zero' = a
+-- @
 --
 --   Commutativity  of '<>':
 --
---   > a <> b = b <> a
+-- @
+--   a '<>' b = b '<>' a
+-- @
 --
 --   Associativity of '><':
 --
---   > a >< (b >< c) = (a >< b) >< c
+-- @
+--   a '><' (b '><' c) = (a '><' b) '><' c
+-- @
 --
 --   Distributivity of '><' over '<>':
 --
---   > a >< (b <> c) = (a >< b) <> (a >< c)
---   > (a <> b) >< c = (a >< c) <> (b >< c)
+-- @
+--   a '><' (b '<>' c) = (a '><' b) '<>' (a '><' c)
+--   (a '<>' b) '><' c = (a '><' c) '<>' (b '><' c)
+-- @
 --
 --   Absorption of '><' by 'zero' (if @s@ is a 'Monoid'):
 --
---   > zero >< a = zero
---   > a >< zero = zero
+-- @
+--   'zero' '><' a = 'zero'
+--   a '><' 'zero' = 'zero'
+-- @
 class Semigroup r => Semiring r where
   infixr 7 ><
   (><) :: r -> r -> r
