a679ede26f5f5c5657475300ddccd77fe6817bd5
Rob Rix <rob.rix@me.com>
2018-05-18 21:35:07 -0400

    :memo: Module.

--- src/Data/Module/Class.hs
+++ src/Data/Module/Class.hs
@@ -3,6 +3,7 @@ module Data.Module.Class where
 
 import Data.Semiring.Class
 
+-- | A left @r@-module over a 'Semiring' @r@.
 class (Semiring r, Semigroup m) => Module r m where
   (><<) :: r -> m -> m
 
