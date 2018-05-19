85b73e26730fdc1b89776e5c59ddc2ae39a0fb91
Rob Rix <rob.rix@me.com>
2018-05-19 15:06:36 -0400

    Derive a bunch of instances.

--- src/Data/Semiring/Few.hs
+++ src/Data/Semiring/Few.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.Few where
 
+import Data.Data (Data(..))
+import Data.Ix (Ix(..))
+import GHC.Generics (Generic(..))
+
 data Few = Zero | One | More
+  deriving (Bounded, Data, Enum, Eq, Generic, Ix, Ord, Show)
