40d3d7e94f2e8cba7142c4f31db91a69b8a34625
Rob Rix <rob.rix@me.com>
2018-05-19 11:19:48 -0400

    Derive a bunch of instances.

--- src/Data/Semiring/App.hs
+++ src/Data/Semiring/App.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.App where
 
+import Data.Data (Data)
+import Data.Ix (Ix)
+import GHC.Generics (Generic, Generic1)
+
 newtype App f a = App { getApp :: f a }
+  deriving (Bounded, Data, Eq, Generic, Generic1, Ix, Ord, Read, Show)
