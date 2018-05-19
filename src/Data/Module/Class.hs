58c1ee1c58d6081d14f0e170df5fc1cdd8090fcf
Rob Rix <rob.rix@me.com>
2018-05-18 21:35:01 -0400

    Require a Semigroup instance for m.

--- src/Data/Module/Class.hs
+++ src/Data/Module/Class.hs
@@ -3,7 +3,7 @@ module Data.Module.Class where
 
 import Data.Semiring.Class
 
-class Semiring r => Module r m where
+class (Semiring r, Semigroup m) => Module r m where
   (><<) :: r -> m -> m
 
   infixl 7 ><<
