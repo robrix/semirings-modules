591d433522355405908672527b42cd01d5cc0efe
Rob Rix <rob.rix@me.com>
2018-05-19 11:18:05 -0400

    Stub in a module for an Applicative Semiring.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,12 +1,14 @@
 -- | Semirings, with and without multiplicative identities.
 module Data.Semiring
-( module Data.Semiring.Arith
+( module Data.Semiring.App
+, module Data.Semiring.Arith
 , module Data.Semiring.Boolean
 , module Data.Semiring.Class
 , module Data.Semiring.Mult
 , module Data.Semiring.Tropical
 ) where
 
+import Data.Semiring.App
 import Data.Semiring.Arith
 import Data.Semiring.Boolean
 import Data.Semiring.Class
