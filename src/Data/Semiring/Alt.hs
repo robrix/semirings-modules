c33e86abe76733c40dde4b0ae3569a850b36e829
Rob Rix <rob.rix@me.com>
2018-05-19 12:34:32 -0400

    Derive a bunch of instances for Alt.

--- src/Data/Semiring/Alt.hs
+++ src/Data/Semiring/Alt.hs
@@ -1,3 +1,9 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.Alt where
 
+import Data.Data (Data)
+import Data.Ix (Ix)
+import GHC.Generics (Generic, Generic1)
+
 newtype Alt f a = Alt { getAlt :: f a }
+  deriving (Bounded, Data, Eq, Generic, Generic1, Ix, Ord, Read, Show)
