a53f6377587dc198ae55af48818db63b76f87e00
Rob Rix <rob.rix@me.com>
2018-05-19 13:03:41 -0400

    Derive a bunch of instances.

--- src/Data/Semiring/Zip.hs
+++ src/Data/Semiring/Zip.hs
@@ -1,3 +1,8 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.Zip where
 
+import Data.Data (Data(..))
+import GHC.Generics (Generic, Generic1)
+
 newtype Zip a = Zip { getZip :: [a] }
+  deriving (Data, Eq, Generic, Generic1, Ord, Read, Show)
