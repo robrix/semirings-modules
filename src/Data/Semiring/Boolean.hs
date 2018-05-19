9363e6c6ac48c28b8c3669ff6c9543ba476e2031
Rob Rix <rob.rix@me.com>
2018-05-18 21:04:17 -0400

    Define a bunch of instances for Bounded.

--- src/Data/Semiring/Boolean.hs
+++ src/Data/Semiring/Boolean.hs
@@ -1,8 +1,25 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
 module Data.Semiring.Boolean
 (
 -- * Boolean algebras
   Boolean(..)
 ) where
 
+import Data.Data (Data(..))
+import Data.Ix (Ix(..))
+import Data.Semiring.Class (Semiring(..), Unital(..))
+import GHC.Generics (Generic)
+
 -- | Boolean algebras form a semiring with '<>' as disjunction, '><' as conjunction, 'zero' as 'False', and 'one' as 'True'.
 newtype Boolean = Boolean { getBoolean :: Bool }
+  deriving (Bounded, Data, Eq, Generic, Ix, Ord, Read, Show)
+
+instance Enum Boolean where
+  succ (Boolean a) = Boolean (succ a)
+  pred (Boolean a) = Boolean (pred a)
+  toEnum = Boolean . toEnum
+  fromEnum = fromEnum . getBoolean
+  enumFrom (Boolean a) = Boolean <$> enumFrom a
+  enumFromThen (Boolean a) (Boolean b) = Boolean <$> enumFromThen a b
+  enumFromTo (Boolean a) (Boolean b) = Boolean <$> enumFromTo a b
+  enumFromThenTo (Boolean a) (Boolean b) (Boolean c) = Boolean <$> enumFromThenTo a b c
