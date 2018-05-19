93c7f8c596aab9aacaabe11513f30ed207d6a67e
Rob Rix <rob.rix@me.com>
2018-05-19 12:32:10 -0400

    Stub in a module for an Alt semiring.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,6 +1,7 @@
 -- | Semirings, with and without multiplicative identities.
 module Data.Semiring
-( module Data.Semiring.App
+( module Data.Semiring.Alt
+, module Data.Semiring.App
 , module Data.Semiring.Arith
 , module Data.Semiring.Boolean
 , module Data.Semiring.Class
@@ -8,6 +9,7 @@ module Data.Semiring
 , module Data.Semiring.Tropical
 ) where
 
+import Data.Semiring.Alt
 import Data.Semiring.App
 import Data.Semiring.Arith
 import Data.Semiring.Boolean
