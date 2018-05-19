fd2e09e550ccf07ebc70d6b53aff2e852f53c9ec
Rob Rix <rob.rix@me.com>
2018-05-18 21:35:43 -0400

    Move the fixity up.

--- src/Data/Module/Class.hs
+++ src/Data/Module/Class.hs
@@ -5,6 +5,5 @@ import Data.Semiring.Class
 
 -- | A left @r@-module over a 'Semiring' @r@.
 class (Semiring r, Semigroup m) => Module r m where
-  (><<) :: r -> m -> m
-
   infixl 7 ><<
+  (><<) :: r -> m -> m
