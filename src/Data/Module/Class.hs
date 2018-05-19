5d82221c89d7c8ce5a7714675487238ca9ae264b
Rob Rix <rob.rix@me.com>
2018-05-18 21:55:28 -0400

    :memo: the Module laws.

--- src/Data/Module/Class.hs
+++ src/Data/Module/Class.hs
@@ -4,6 +4,41 @@ module Data.Module.Class where
 import Data.Semiring.Class
 
 -- | A left @r@-module over a 'Semiring' @r@.
+--
+--   Laws:
+--
+--   Associativity of '<>' (the 'Semigroup' law):
+--
+-- @
+--   a '<>' (b '<>' c) = (a '<>' b) '<>' c
+-- @
+--
+--   Identity of '<>' (the 'Monoid' law, if @m@ is a 'Monoid'):
+--
+-- @
+--   'zero' '<>' a    = a
+--   a    '<>' 'zero' = a
+-- @
+--
+--   Left-distributivity of '><<' over @m@ '<>':
+--
+-- @
+--   r '><<' (x '<>' y) = r '><<' x '<>' r '><<' y
+-- @
+--
+--   Left-distributivity of @r@ '<>' over '><<':
+--
+-- @
+--   (r '<>' s) '><<' x = r '><<' x '<>' s '><<' x
+-- @
+--
+--   Left-distributivity of '><' over '><<':
+--
+-- @
+--   (r '><' s) '><<' x = r '><<' (s '><<' x)
+-- @
+--
+--   Left-identity of '><<', if @r@ is 'Unital'.
 class (Semiring r, Semigroup m) => Module r m where
   infixl 7 ><<
   (><<) :: r -> m -> m
